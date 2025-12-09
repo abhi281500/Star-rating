481abe43b52c2600e28c8b15db7bac37e2a596c9
abhi281500 <abhi281500@gmail.com>
2025-12-09 20:07:05 +0530

    project

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import {FaStar} from 'react-icons/fa'
+import React, { useState } from 'react'
+import './App.css'
+
+function App( {noofStars = 5} ) {
+  const [rating, setRating] =useState(0);
+  const [hover, setHover] = useState(null);
+
+  function handleclick(getcurrentindex) {
+    
+    setRating(getcurrentindex + 1);
+    
+  }
+  function handleMouseMove(getcurrentindex) {
+    
+    setHover(getcurrentindex + 1);
+  }
+  function handleMouseLeave() {
+    
+    setHover(null);
+  }
+  
+  return (
+    <>
+        <h2>Star Rating Component</h2>
+
+        {
+          [...Array(noofStars)].map((_, index) => {
+            return <FaStar
+             key={index} 
+             
+             onClick={() => handleclick(index)}
+             onMouseMove={() => handleMouseMove(index)}
+             onMouseLeave={() => handleMouseLeave()}
+              color={(hover || rating) > index ? "#ffc107" : "#e4e5e9"}
+              size={40}
+             
+             />
+        
+        
+        
+        
+          })
+        }
+        
+
+
+    </>
+  )
+}
+
+export default App
